3b84e92de57496e98e38de11a9519613dfa60992
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-11-03 10:27:20 +0200

    added more apis

--- APIs/order.js
+++ APIs/order.js
@@ -1,16 +1,52 @@
 const orderAPI = (app, con) =>{
 
+  app.get('/orders/count', (req, res) => {
+    const sql = `
+    SELECT COUNT(*) AS ordersRows
+    FROM _order`;
+    con.query(sql, (err, result) => {
+      if(err) throw err;
+      res.send(result);
+    });
+  })
+
+  app.get('/orders/sum', (req, res) => {
+    const sql = `
+    SELECT SUM(price) AS ordersSum
+    FROM _order
+    LEFT JOIN clothes AS cl
+    ON _order.clothes_id = cl.id`;
+    con.query(sql, (err, result) => {
+      if(err) throw err;
+      res.send(result);
+    });
+  })
+
+  // app.get('/orders', (req, res) => {
+  //     const sql = `
+  //     SELECT _order.*, cl.type, cl.color, cl.price 
+  //     FROM _order
+  //     LEFT JOIN clothes AS cl
+  //     ON _order.clothes_id = cl.id
+  //     `;
+  //     con.query(sql, (err, result) => {
+  //       if(err) throw err;
+  //       res.send(result);
+  //     });
+  // })
+
   app.get('/orders', (req, res) => {
-      const sql = `
-      SELECT _order.*, cl.type, cl.color, cl.price 
-      FROM _order
-      LEFT JOIN clothes AS cl
-      ON _order.clothes_id = cl.id
-      `;
-      con.query(sql, (err, result) => {
-        if(err) throw err;
-        res.send(result);
-      });
+    const sql = `
+    SELECT _order.*, cl.type, cl.color, cl.price 
+    FROM _order
+    LEFT JOIN clothes AS cl
+    ON _order.clothes_id = cl.id
+    LIMIT ${req.query.page * 10}, 10
+    `;
+    con.query(sql, [req.params.start], (err, result) => {
+      if(err) throw err;
+      res.send(result);
+    });
   })
 
   app.put('/orders/:id', (req, res) => {
